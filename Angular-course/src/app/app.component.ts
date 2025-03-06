3ece16dde64d3d948f74275ec71ad37e055e300f
sravya2001-sys <klakshmisravya21@gmail.com>
2025-03-06 20:25:18 +0530

    Task Component,Output() practice

--- Angular-course/src/app/app.component.ts
+++ Angular-course/src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
+import { Users } from './users';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-course';
+  users = DUMMY_USERS;
+  selectedUserid = "u1";
 
+  get selectedUser()
+  {
+      //alert(this.selectedUserid);
+      return this.users.find((user)=>user.id === this.selectedUserid)!;
+  }
+  onRecieveChildData(id:string)
+  {
+      this.selectedUserid = id;
+      //alert(this.selectedUserid);
+  }
 }
--- Angular-course/src/app/user/user.component.ts
+++ Angular-course/src/app/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component , signal,computed} from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
+import { Component,Input,Output,EventEmitter } from '@angular/core';
+import { Users } from '../users';
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -8,19 +8,13 @@ const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-current_user = signal(DUMMY_USERS[randomIndex]);
-//defining a getter-->this behaves like a property in template
-// get imagePath(){
-//   return 'assets/users/'+this.current_user.avatar;
-// }
-imagePath = computed(()=>'assets/users/'+this.current_user().avatar);
-/* 
-why not use like this ? imagePath = 'assets/users/'+this.current_user().avatar;
-as current_user is a signal here , computed() method will revaluate imagePath whenever this signal value changes and returns signal in return, in short is subscribes to signal and recomputes when ever signal is updated
-*/
+// @Input({required:true}) avatar!:string;
+// @Input({required:true}) name!:string;
+// @Input({required:true}) id!:string;
+@Input({required:true}) user!:Users;
+@Output() select = new EventEmitter<string>();
 
 onUserSelected(){
-  const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
-  this.current_user.set(DUMMY_USERS[randomIndex]);
+  this.select.emit(this.user.id);
 }
 }
